d9a60f00d66fe3a307b5f7e049484a8e46a7dccc
Jarek Radosz <jradosz@gmail.com>
2017-12-30 03:45:36 +0100

    Leak notes.

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,3 +10,17 @@ export default Route.extend({
     this.get('test');
   },
 });
+
+// Elements required to leak memory:
+// 1. A computed property that reads from
+// 2. an injected service
+// 3. that is a proxy
+// 4. to `getOwner(this).factoryFor('config:environment').class`
+// 5. in a FastBoot server
+//
+// ad 1. doing `this.get('config.test')` directly does not leak memory
+// ad 2. creating proxy directly in Route's init() method does not leak memory
+//   (i.e. `this.set('config', ObjectProxy.create({ content: getOwner(this).factoryFor('config:environment').class }));`)
+// ad 3. replacing ObjectProxy with EmberObject (and then reading from 'config.content.test') does not leak memory
+// ad 4. setting proxy content to a direct config import (i.e. `import env from '../config/environment'; this.set('content', env);`) does not leak memory
+// ad 5. it seems that the app instance (or some part of it) is leaked, so unless new instances are created - there's no "leak"
